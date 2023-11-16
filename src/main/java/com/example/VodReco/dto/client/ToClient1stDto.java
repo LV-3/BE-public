64a6945a4c88f6ec664a80d2968b7c280a83d8fb
workingoniit <knsy2465@gmail.com>
2023-11-16 11:38:23 +0900

    client, model과 주고받는 데이터 형식 수정

--- src/main/java/com/example/VodReco/dto/client/ToClient1stDto.java
+++ src/main/java/com/example/VodReco/dto/client/ToClient1stDto.java
@@ -4,15 +4,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 public class ToClient1stDto {
     private String contentId;
     private String posterurl;
+    private List<String> mood;
 
     @Builder
-    public ToClient1stDto(String contentId, String posterurl) {
+    public ToClient1stDto(String contentId, String posterurl, List<String> mood) {
         this.contentId = contentId;
         this.posterurl = posterurl;
+        this.mood = mood;
     }
 }
---
+++ src/main/java/com/example/VodReco/mongoRepository/TotalRepository.java
@@ -0,0 +1,8 @@
+package com.example.VodReco.mongoRepository;
+
+import com.example.VodReco.domain.Total;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TotalRepository extends MongoRepository<Total, String> {
+
+}
