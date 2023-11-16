160e0dd23466fff435ba4cc3d67a3a483bfdbf41
workingoniit <knsy2465@gmail.com>
2023-11-16 11:39:57 +0900

    kafka topic에 넘길 데이터 형식 수정

--- src/main/java/com/example/VodReco/dto/model/ToModelDto.java
+++ src/main/java/com/example/VodReco/dto/model/ToModelDto.java
@@ -1,18 +1,17 @@
 package com.example.VodReco.dto.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Component // 확인 필요(231112)
 public class ToModelDto {
-    String modelName;
-    List<?> responseData; //필드명이 헷갈림. 추후 수정(231113)
+    private String modelName;
+    private List<?> responseData; //필드명이 헷갈림. 추후 수정(231113)
 }
