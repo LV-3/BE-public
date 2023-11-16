3871d86d2c5e53ac25a5a981a0119398f59819a6
workingoniit <knsy2465@gmail.com>
2023-11-16 11:40:12 +0900

    subsr login 적용

--- src/main/java/com/example/VodReco/repository/UserRepository.java
+++ src/main/java/com/example/VodReco/repository/UserRepository.java
@@ -2,17 +2,11 @@ package com.example.VodReco.repository;
 
 
 import com.example.VodReco.domain.User;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
-@Repository // ??????????
-public interface UserRepository extends JpaRepository<User, Long> {
-    @EntityGraph(attributePaths = "authorities")
-    Optional<User> findOneWithAuthoritiesByEmail(String email); //orElse(null) 쓰려고 Optional로 감싸서 리턴
-
-    @EntityGraph(attributePaths = "authorities")
-    Optional<User> findOneWithAuthoritiesByNickname(String nickname); //orElse(null) 쓰려고 Optional로 감싸서 리턴
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findBySubsr(String subsr);
 }
