15157008efd820023b9d9e1c855d496643f05b0c
workingoniit <knsy2465@gmail.com>
2023-11-16 11:06:03 +0900

    remove: Spring Security, jwt 설정 삭제

--- src/main/java/com/example/VodReco/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.VodReco.repository;
-
-import com.example.VodReco.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository //이거 왜 소스코드에 없지 당연히 있어야 되는 게 아니었나?
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
