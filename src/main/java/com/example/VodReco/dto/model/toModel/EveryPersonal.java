c83933a4eac991ddc7f50d4b3c5842229ef3034e
workingoniit <knsy2465@gmail.com>
2023-11-26 01:25:35 +0900

    rename:vodDetailPage 내 패키지 wish, rating 분리

---
+++ src/main/java/com/example/VodReco/dto/model/toModel/EveryPersonal.java
@@ -0,0 +1,2 @@
+package com.example.VodReco.dto.model.toModel;public class EveryPersonalDto {
+}
---
+++ src/main/java/com/example/VodReco/service/mainPage/viewVodsByMood/VodviewVodsMyMoodService.java
@@ -0,0 +1,2 @@
+package com.example.VodReco.service.mainPage.viewVodsByMood;public interface VodviewVodsMyMoodService {
+}
