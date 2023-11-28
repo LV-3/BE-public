3eae58dc9425d00c8c370661e869fbfd4fcd0cbe
workingoniit <knsy2465@gmail.com>
2023-11-28 09:48:26 +0900

    fix:login API 리턴 수정

--- src/main/java/com/example/VodReco/controller/UserLoginController.java
+++ src/main/java/com/example/VodReco/controller/UserLoginController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 public class UserLoginController {
@@ -18,11 +20,12 @@ public class UserLoginController {
     // [재아] 로그인 성공, 실패 나눠서 구현
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
-        String loginResult = userLoginService.ValidateUser(loginDto);
-        if (loginResult.equals("success")) {
-            return ResponseEntity.ok().body("Login successful");
+        //ValidateUser의 리턴에 맞게 equals 내부 값 수정(231128)
+        //요구사항에 맞게 리턴 null로 수정(231128)
+        if (userLoginService.ValidateUser(loginDto).equals(loginDto.getSubsr())) {
+            return ResponseEntity.ok().body(userLoginService.ValidateUser(loginDto));
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
 
