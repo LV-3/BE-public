a65b3174138b6be58bf4e70356f680471e845886
workingoniit <knsy2465@gmail.com>
2023-11-30 08:48:44 +0900

    테스트 코드 주석처리

--- src/test/java/com/example/VodReco/mongoRepository/GenresRepositoryTest.java
+++ src/test/java/com/example/VodReco/mongoRepository/GenresRepositoryTest.java
@@ -1,17 +1,17 @@
-package com.example.VodReco.mongoRepository;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class GenresRepositoryTest {
-
-    @Test
-    void findAllBy() {
-        //given
-        //when
-
-        //then
-    }
-
-}
+//package com.example.VodReco.mongoRepository;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class GenresRepositoryTest {
+//
+//    @Test
+//    void findAllBy() {
+//        //given
+//        //when
+//
+//        //then
+//    }
+//
+//}
