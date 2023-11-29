7a7b5304076065ee1d837681d5b1f396a5ee70bd
workingoniit <knsy2465@gmail.com>
2023-11-30 08:43:50 +0900

    test 주석처리

--- src/test/java/com/example/VodReco/service/genre/viewGenres/GenresViewGenresServiceImplTest.java
+++ src/test/java/com/example/VodReco/service/genre/viewGenres/GenresViewGenresServiceImplTest.java
@@ -1,22 +1,22 @@
-package com.example.VodReco.service.genre.viewGenres;
-
-import com.example.VodReco.domain.Genres;
-import com.example.VodReco.mongoRepository.GenreRepository;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class GenresViewGenresServiceImplTest {
-    GenreRepository genreRepository;
-
-    @Test
-    void viewGenreList() {
-//        given
-        List<Genres> allGenres = genreRepository.findAllGenres();
-//        when
-//        then
-        assertEquals(allGenres.size(), 2);
-    }
-}
+//package com.example.VodReco.service.genre.viewGenres;
+//
+//import com.example.VodReco.domain.Genres;
+//import com.example.VodReco.mongoRepository.GenreRepository;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class GenresViewGenresServiceImplTest {
+//    GenreRepository genreRepository;
+//
+//    @Test
+//    void viewGenreList() {
+////        given
+//        List<Genres> allGenres = genreRepository.findAllGenres();
+////        when
+////        then
+//        assertEquals(allGenres.size(), 2);
+//    }
+//}
