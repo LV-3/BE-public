45857e200d5b76881c0ad10341f588d955f56380
workingoniit <knsy2465@gmail.com>
2023-12-01 09:16:52 +0900

    application.yml 수정

--- src/main/java/com/example/VodReco/VodRecoApplication.java
+++ src/main/java/com/example/VodReco/VodRecoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication // 이 어노테이션 안에 @ComponentScan 들어있음 = 하위 모두 ComponentScan의 대상
 @EnableMongoRepositories(basePackages = "com.example.VodReco.mongoRepository")
 @EnableJpaRepositories(basePackages = "com.example.VodReco.repository") // JpaRepository 패키지 위치 등록
-public class VodRecoApplication {
+public class
+VodRecoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VodRecoApplication.class, args);
