80a89eced19a88f3bf454a2cce0fb80e72d8a96b
ah-zae <jaeah2171@gmail.com>
2023-12-01 12:16:56 +0900

    update return code -> for error code

--- src/main/java/com/example/VodReco/controller/MypageController.java
+++ src/main/java/com/example/VodReco/controller/MypageController.java
@@ -29,32 +29,32 @@ public class MypageController {
         this.userWatchListViewService = userWatchListViewService;
     }
     // [jjae] - 코드 변경 : rating, wish, replay -> 모두 리턴 타입 변경 + 메소드형식(?) 변경
-    //[세연] 실패한 경우와 리턴 분리 요망(231128)
+    // [세연] 실패한 경우와 리턴 분리 요망(231128) --> [jjae] 주석 제거 시 해당 vod가 없는 경우 '콘텐츠 없음'을 반환
 
     @PostMapping("/rating")
     public ResponseEntity<List<ViewMyRatingListDto>> findMyRatingList(@RequestBody UserDto userDto){
         Optional<List<ViewMyRatingListDto>> myRatingListDtos = userRatingListViewService.findMyRatingList(userDto.getSubsr());
-//        if (!myRatingListDtos.isPresent() || myRatingListDtos.get().isEmpty()) {
-//            return ResponseEntity.noContent().build();
-//        }
+        if (!myRatingListDtos.isPresent() || myRatingListDtos.get().isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(myRatingListDtos.get());
     }
 
     @PostMapping("/wish")
     public ResponseEntity<List<ViewMyWishListDto>> findMyWishList(@RequestBody UserDto userDto){
         Optional<List<ViewMyWishListDto>> myWishListDtos = userWishListViewService.findMyWishList(userDto.getSubsr());
-//        if (!myWishListDtos.isPresent() || myWishListDtos.get().isEmpty()) {
-//            return ResponseEntity.noContent().build();
-//        }
+        if (!myWishListDtos.isPresent() || myWishListDtos.get().isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(myWishListDtos.get());
     }
 
     @PostMapping("/replay")
     public ResponseEntity<Optional<List<ViewMyWatchListDto>>> findMyWatchList(@RequestBody UserDto userDto){
         Optional<List<ViewMyWatchListDto>> myWatchListDtos = userWatchListViewService.findMyWatchList(userDto.getSubsr());
-//        if (!myWatchListDtos.isPresent() || myWatchListDtos.get().isEmpty()) {
-//            return ResponseEntity.noContent().build();
-//        }
+        if (!myWatchListDtos.isPresent() || myWatchListDtos.get().isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(myWatchListDtos);
     }
 
