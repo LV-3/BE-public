149df379e7880ab3d7169c33979d3e1fb4898753
workingoniit <knsy2465@gmail.com>
2023-12-04 11:06:28 +0900

    fix:send TITLE_SMRY instead of SMRY if series

--- src/main/java/com/example/VodReco/service/genre/viewVodsByGenre/VodViewVodsByGenreServiceImpl.java
+++ src/main/java/com/example/VodReco/service/genre/viewVodsByGenre/VodViewVodsByGenreServiceImpl.java
@@ -26,7 +26,6 @@ public class VodViewVodsByGenreServiceImpl implements VodViewVodsByGenreService{
     @Override
     public List<BasicInfoOfVodDto> viewVodsByGenre(String genre) throws UnsupportedEncodingException {
         List<BasicInfoOfVodDto> list = new ArrayList<>();
-        //?? 아래 두 줄 다 "한글 장르" 출력됨. 애초에 디코딩 필요없이 그대로 들어옴
 //        String decodedGenre = URLDecoder.decode(genre, "utf-8");
 //        System.out.println(decodedGenre); // "한글 장르" 출력됨
 //        System.out.println("디코딩 전 = " + genre);
