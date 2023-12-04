02b46510da2e699b89d4234ccaee358a99b3d719
workingoniit <knsy2465@gmail.com>
2023-12-04 18:26:25 +0900

    refactor:LoggingService, LoggingRepository  클래스 전체 주석처리

--- src/main/java/com/example/VodReco/service/logging/LoggingService.java
+++ src/main/java/com/example/VodReco/service/logging/LoggingService.java
@@ -1,15 +1,15 @@
-package com.example.VodReco.service.logging;
-
-import com.example.VodReco.domain.LogEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-
-public interface LoggingService
-{
-    @Transactional
-    void saveLog(LogEntity logEntity);
-
-    void saveLogConsole(LogEntity log);
-}
+//package com.example.VodReco.service.logging;
+//
+//import com.example.VodReco.domain.LogEntity;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//
+//public interface LoggingService
+//{
+//    @Transactional
+//    void saveLog(LogEntity logEntity);
+//
+//    void saveLogConsole(LogEntity log);
+//}
