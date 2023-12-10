0109dc89978c612898e0ee7f797afda6e92c90c7
workingoniit <knsy2465@gmail.com>
2023-12-11 04:02:08 +0900

    Feat: return tags list

--- src/main/java/com/example/VodReco/service/mainPage/getReco/VodGetRecoService.java
+++ src/main/java/com/example/VodReco/service/mainPage/getReco/VodGetRecoService.java
@@ -1,16 +1,14 @@
 package com.example.VodReco.service.mainPage.getReco;
 
 import com.example.VodReco.dto.client.MainResponseDto;
-import com.example.VodReco.dto.model.toModel.ToModelDto;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public interface VodGetRecoService {
 
 
-    MainResponseDto getAllContentIdsFromModel(String subsr);
+    Mono<MainResponseDto> getAllContentIdsFromModel(String subsr);
 
-    ToModelDto setDataForModel(String subsr);
 
-    void parse(String recoResult);
 }
