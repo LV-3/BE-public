e1995a5836bfbb1c95c344a3515d1092d8d24856
workingoniit <knsy2465@gmail.com>
2023-12-16 09:42:16 +0900

    Chore: change /weatherRec endpoint to /weather

--- src/main/java/com/example/VodReco/controller/MainController.java
+++ src/main/java/com/example/VodReco/controller/MainController.java
@@ -2,10 +2,12 @@ package com.example.VodReco.controller;
 
 import com.example.VodReco.aspects.LogExecutionTime;
 import com.example.VodReco.dto.UserDto;
+import com.example.VodReco.dto.WeatherRecDto;
 import com.example.VodReco.dto.client.MainResponseDto;
 import com.example.VodReco.dto.BasicInfoOfVodDto;
 import com.example.VodReco.service.mainPage.getReco.VodGetRecoServiceImpl;
 import com.example.VodReco.service.mainPage.viewVodsByTag.VodviewVodsByTagServiceImpl;
+import com.example.VodReco.service.viewWeatherRec.WeatherRecViewServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
@@ -28,27 +30,33 @@ public class MainController {
 
     private final ObjectMapper objectMapper;
 
+    private final WeatherRecViewServiceImpl weatherRecViewService;
+
 
     @Lazy
-//    @Autowired
+
+    //    @Autowired
 //    두 개 붙으면 Lazy가 이김
     public MainController(VodviewVodsByTagServiceImpl vodviewVodsByMoodService, VodGetRecoServiceImpl vodGetRecoService,
-                          ObjectMapper objectMapper) {
+                          ObjectMapper objectMapper, WeatherRecViewServiceImpl weatherRecViewService) {
         this.vodviewVodsByMoodService = vodviewVodsByMoodService;
         this.vodGetRecoService = vodGetRecoService;
 
         this.objectMapper = objectMapper;
+        this.weatherRecViewService = weatherRecViewService;
     }
 
     @LogExecutionTime
     @PostMapping("")
 //    public ResponseEntity<Mono<MainResponseDto>> getAllRecoFromModel(@RequestBody UserDto userDto) {
 //    public ResponseEntity<MainResponseDto> getAllRecoFromModel(@RequestBody UserDto userDto) {
+
 //    public Mono<MainResponseDto> getAllRecoFromModel(@RequestBody UserDto userDto) {
     public MainResponseDto getAllRecoFromModel(@RequestBody UserDto userDto) {
+
 //        if (vodGetRecoService.getAllContentIdsFromModel(userDto.getSubsr()) != null) {
 //            return ResponseEntity.ok(vodGetRecoService.getAllContentIdsFromModel(userDto.getSubsr()));
-            return vodGetRecoService.getAllContentIdsFromModel(userDto.getSubsr());
+        return vodGetRecoService.getAllContentIdsFromModel(userDto.getSubsr());
 //        }
 //        //에러코드 204
 //            return ResponseEntity.noContent().build();
@@ -58,7 +66,7 @@ public class MainController {
 
     //태그(템플릿 단어)별 vod 조회
     @GetMapping("/{tags}")
-    public ResponseEntity<List<BasicInfoOfVodDto>> sendEachTagVods (@PathVariable String tags){
+    public ResponseEntity<List<BasicInfoOfVodDto>> sendEachTagVods(@PathVariable String tags) {
         if (vodviewVodsByMoodService.sendEachTagVods(tags).isEmpty()) {
             //에러코드 204
             return ResponseEntity.noContent().build();
@@ -67,8 +75,11 @@ public class MainController {
     }
 
 
-
-
+    //날씨 기반 추천
+    @GetMapping("/weather")
+    public WeatherRecDto sendWeatherRecs() {
+        return weatherRecViewService.sendWeatherRecommendations();
+    }
 }
 
 
