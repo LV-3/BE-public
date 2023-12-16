38f128d1548a2d941d355372761d1b35a9e00419
workingoniit <knsy2465@gmail.com>
2023-12-16 11:40:47 +0900

    Fix: /popular API 리턴 contentId → content_id

--- src/main/java/com/example/VodReco/dto/popular/ViewPopularVodsDto.java
+++ src/main/java/com/example/VodReco/dto/popular/ViewPopularVodsDto.java
@@ -1,10 +1,12 @@
 package com.example.VodReco.dto.popular;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class ViewPopularVodsDto {
+    @JsonProperty("content_id")
     private String contentId;
     private String title;
     private String posterurl;
